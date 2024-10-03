34f8e5b2af5febee1c6f7b59ee068b704fcf328f
jaykal12 <rekahnohtyp@gmail.com>
2024-10-03 20:18:56 +0530

    Add files via upload

--- hhwi.py
+++ hhwi.py
@@ -14,7 +14,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 def display_images():
     images = os.listdir(UPLOAD_FOLDER)
     if images:
-        st.subheader("Binod ka message hai ")
+        st.subheader("Binod ka message hai --> read time 45 minutes")
         for image in images:
             img = Image.open(os.path.join(UPLOAD_FOLDER, image))
             st.image(img, caption=image, use_column_width=True)
